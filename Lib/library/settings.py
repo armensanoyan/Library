fdf9c412485762adfcbb3edc27aa21155daafb6e
Armen Sanoyan <armensanoyan@gmail.com>
2020-12-12 22:14:34 +0400

    DB moved from sqlite to postgresql

--- Lib/library/settings.py
+++ Lib/library/settings.py
@@ -82,8 +82,20 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'libo',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'kak000',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
     }
 }
 
