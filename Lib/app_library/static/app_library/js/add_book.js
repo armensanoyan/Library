24ce77ecfdebcf3c1c18929e5635763f153d67aa
Armen Sanoyan <armensanoyan@gmail.com>
2020-12-17 13:19:13 +0400

    used csrf token for securly sending data from broser to server

--- Lib/app_library/static/app_library/js/add_book.js
+++ Lib/app_library/static/app_library/js/add_book.js
@@ -1,13 +1,25 @@
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
 function send_book(id) {
 	xml = new XMLHttpRequest()
-	xml.open('GET', `http://localhost:8000/user_book?q=${id}`)
+	xml.open('POST', `http://localhost:8000/user_book`)
 	xml.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-	// xml.onreadystatechange = function() { 
-	//     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-	//     }
-	// }
-	xml.send();
+	xml.setRequestHeader('X-CSRFToken', csrftoken);
+	xml.send(`name=${id}`);
 }
 
 function add_book(event) {
