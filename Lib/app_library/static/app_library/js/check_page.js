64e1c54afe38265a518c8b98dbfcbc2ecc1632ab
Armen Sanoyan <armensanoyan@gmail.com>
2020-12-20 14:39:48 +0400

    removed logs

--- Lib/app_library/static/app_library/js/check_page.js
+++ Lib/app_library/static/app_library/js/check_page.js
@@ -5,11 +5,9 @@ icons = document.getElementsByClassName('manubaricon')
 if (title == 'Profile') {
 	for(let i=0; i<2; i++) {
 		icons[i].style.display="none"
-		console.log(icons[i])
 	}
 } else {
 	for(let i=0; i<2; i++) {
 		icons[i].style.display="inline"
-		console.log(icons[i])
 	}
 }
