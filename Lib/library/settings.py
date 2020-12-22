9c3306a149d371a7f596f9a4035f3e9a2ad8aa9f
Armen Sanoyan <armensanoyan@gmail.com>
2020-12-22 14:20:37 +0400

    added logging to settings

--- Lib/library/settings.py
+++ Lib/library/settings.py
@@ -122,4 +122,43 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 import django_heroku
-django_heroku.settings(locals())
+django_heroku.settings(locals(),logging=False)
+TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
+
+import logging
+logger = logging.getLogger('testlogger')
+logger.info('This is a simple log message')
